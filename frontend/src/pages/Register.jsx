46b0c88cf6dc0d5aa7c523890668283973726a66
Sayan <sayanmunshi36@gmail.com>
2022-06-12 17:34:49 +0530

    Initial Frontend Setup Done

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, password, password2 } = formData;
+
   return (
     <div>
       <h1>Register</h1>
