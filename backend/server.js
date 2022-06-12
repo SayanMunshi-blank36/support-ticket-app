ca51f7a3026f01d69055e58e4e8e54da95e75e38
Sayan <sayanmunshi36@gmail.com>
2022-06-12 11:29:15 +0530

    User Routes Setup, User Controller Setup, ErrorMiddleware setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,22 @@
-console.log("Server");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+// Body Parser Middleware for getting form values
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Welcome to the Support Desk API" });
+});
+
+// Routes
+app.use("/api/users", require("./routes/userRoutes"));
+
+// Error Middleware
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
